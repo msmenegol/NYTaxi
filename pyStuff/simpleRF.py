26d29216d627be5e4b576acc4422e3e21060003c
msmenegol <marcelosmenegol@gmail.com>
2017-08-11 21:31:44 -0300

    Rearrange things for kaggle submission

--- pyStuff/simpleRF.py
+++ pyStuff/simpleRF.py
@@ -1,12 +1,13 @@
+#Python version of the Last Place Benchmark in R, by Laura: https://www.kaggle.com/mrisdal/last-place-laura-benchmark
+
 import time
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from datetime import datetime as dt
 
 #Time up your execution
 start_time = time.time()
 
+####SUMMARY OF THE DATA#####
 #Load the data
 train_data = pd.read_csv('../train.csv')
 test_data = pd.read_csv('../test.csv')
@@ -18,33 +19,42 @@ test_column_names = list(test_data.axes[1])
 print(test_column_names)
 
 #get a summary of the training data
-train_data.describe(include='all') #by default, describe() only returns numerical columns.
+print(train_data.describe(include='all')) #by default, describe() only returns numerical columns.
+
+####PLOT TRIP DURATION####
+#Unfortunately,
+import matplotlib.pyplot as plt
+from datetime import datetime as dt
 
 #get a sample of the data
-sample = train_data.sample(n=10000, random_state=64)
-
-def add_jitter(arr):
-    return arr + (np.random.random(len(arr))-0.5)*arr.std()*0.1
+sample = train_data.sample(n=10000, random_state=412)
 
 pickup_hour = np.array([dt.strptime(x, '%Y-%m-%d %H:%M:%S').hour for x in sample['pickup_datetime'].values])
 
-#plot data
+#There is no native "jitter" option in matplotlib, so we make our own
+def add_jitter(arr):
+    return arr + (np.random.random(len(arr))-0.5)*arr.std()*0.1
+
+#Get information of your screen to display a reazonably sized graph
+#Uncomment it when you run it on your computer
+'''
 import tkinter as tk
 window = tk.Tk() #This opens a window
 screen_x = np.floor(window.winfo_screenmmwidth()/25.4)
 screen_y = np.floor(window.winfo_screenmmheight()/25.4)
 window.destroy() #This destroys the extra window
-
 plt.figure(figsize=[screen_x*0.75, screen_y*0.75])
+'''
+
+#Set the graph and plot it
 plt.plot(add_jitter(pickup_hour), np.log(sample['trip_duration'].values), 'r.')
 plt.xticks(np.arange(24))
 plt.xlabel('Hour of Pickup')
 plt.ylabel('Log of Trip Duration')
 plt.title('Trip Duration by Pickup Hour')
-#plt.rcParams["figure.figsize"]=[screen_x*0.75, screen_y*0.75]
 plt.show()
 
-#Tackle the problem with a random forest
+####MAKE YOUR FIRST PREDICTION####
 import sklearn.ensemble
 
 #create classifier
